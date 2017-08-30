3899fee5b1e1f3f133226957b17bdccf523165d7
yclog <mr.wang.cheng.yuan@gmail.com>
2017-08-30 13:00:50 +0800

    stable version 1.1.0

---
+++ app/src/main/java/com/self/ylog/subway/SubwayApplication.java
@@ -0,0 +1,14 @@
+package com.self.ylog.subway;
+
+import android.app.Application;
+/**
+ * Created by deer on 2017/8/29.
+ */
+
+public class SubwayApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
